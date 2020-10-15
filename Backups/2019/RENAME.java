727cd1a1ef14859e9e7d2d04af22fa7593ee8cfb
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-14 20:56:06 -0700

    RENAME.java
    information on a future file move.

---
+++ Backups/2019/RENAME.java
@@ -0,0 +1,16 @@
+// For when I learn how to mass-move binary data in Git
+// Directories and their new names and locations
+
+/* Backups 18 and 19
+* /7.17.2020/
+*/
+
+/* Backup 20 and 21
+* /7.18.2020/
+*/
+
+/* Backup 22
+/ /7.19.2020.
+*/
+
+// End
