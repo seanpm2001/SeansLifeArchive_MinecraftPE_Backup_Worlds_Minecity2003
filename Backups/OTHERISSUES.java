84800101ca21f3482b8dfd0d2d74f5a4c116ddde
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-14 21:09:10 -0700

    OTHERISSUES.java
    Other issues for this project.

---
+++ Backups/OTHERISSUES.java
@@ -0,0 +1,6 @@
+/*
+* Other issues during the GitHub conversion:
+* I forgot to put some directories within a day directory, and in-turn, a month directory
+* The 2019 directory shouldn't exist, all files were originally formatted in the root of the main directory
+* I need to know how to move binary files en masse in Java
+*/
